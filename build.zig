24642dae9d7efb42491c5e4eb45849f02a6f7d4c
Hassan-Ibrahim-1 <hassanibrahimasi@gmail.com>
2025-12-29 13:23:40 -0500

    build system init

---
+++ build.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libcpp = true,
+    });
+
+    exe_mod.addCSourceFiles(.{
+        .files = &.{
+            "src/main.cpp",
+        },
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "norm",
+        .root_module = exe_mod,
+    });
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+}
